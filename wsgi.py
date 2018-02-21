ac953a21e5e1f2c6326a315613ed963d7840e09b
ArktinenSieni <matti.raty@helsinki.fi>
2018-02-21 10:45:31 +0200

    logging

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,53 @@
 import socket
+import datetime
+import file
+import logging
 from flask import Flask
 
 application = Flask(__name__)
 
+
+def get_logfile():
+    return "/mnt/important_logs"
+
+
+def read_log():
+    try:
+        log_file = file.open(get_logfile, "r")
+        logging = log_file.read()
+        log_file.close()
+
+        logging.info("Log reading successful")
+    
+    except Exception as e:
+        logging.warning("Log reading failed: " + e)
+
+
+def write_log():
+    try:
+        log_file = file.open(get_logfile(), "a")
+        log_line = socket.gethostname + " " + str(datetime.datetime.now())
+
+        log_file.write(log_line)
+        log_file.close()
+
+        logging.info("Logging successful")
+    except Exception as e:
+        logging.warning("Logging failed: " + e)
+
+
+
 @application.route("/")
 def hello():
+
+    showed_output = str(
+        "Hello World! Greetings from ",
+        socket.gethostname(),
+        "\n",
+        read_log
+    )
     
-    return "Hello World! Greetings from "+socket.gethostname()+"\n"
+    return showed_output
 
 
 if __name__ == "__main__":
